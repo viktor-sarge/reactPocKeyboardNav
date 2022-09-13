5f22f5ea524e9b7de4c30458ccb6b77fb43329ac
Viktor Sarge <viktor.sarge@regionhalland.se>
2022-09-13 14:36:51 +0300

    feat(keyboard): ✨ add initial poc of using refs and onKeyDown as base for keyboard nav
    Snabb POC av att använda refs och onKeyDown som bas i tangentbordsnavigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {useRef, useState} from 'react';
+
 
 function App() {
+
+  const [navbarData, setNavbarData] = useState([
+    {name: "Dimma där", id:crypto.randomUUID(), ref:useRef()},
+    {name: "Ånyo hinsides", id:crypto.randomUUID(), ref:useRef()},
+    {name: "Alldenstund åsyn", id:crypto.randomUUID(), ref:useRef()}
+  ]);
+
+  function handleKeyPress(e) {
+    console.log("This should only display when keyDown on the links"); // Bara dubbelkollar att det inte triggar oftare än det skall  
+    const activeNodeTest = (element) => element.id == e.target.id;  // Hittar den nod vars id matchar id på den nod som triggad eventet
+    const activeNodeIndex = navbarData.findIndex(activeNodeTest);  // Ger index för den nod som hittades ovan
+    const nodeToFocus = activeNodeIndex + 1 >= navbarData.length ? 0 : activeNodeIndex + 1; // Säkrar att vi börjar om från första noden
+    navbarData[nodeToFocus].ref.current.focus();  // Sätter fokus på nästa nod
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul> 
+        {navbarData.map(node=>
+          <li key={node.id}>
+            <a ref={node.ref} id={node.id} onKeyDown={handleKeyPress} href="regionhalland.se">{node.name}</a>
+          </li>)
+        }
+      </ul>
     </div>
   );
 }
